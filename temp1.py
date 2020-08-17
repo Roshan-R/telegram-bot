392592f38b3b289bf3be0391598bfa7f8b8ada79
Roshan-R <roshanr2001@gmail.com>
2020-08-17 12:18:28 +0530

    hosting to heroku

--- temp1.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-thing = input("Enter the thing : ")
-url = 'https://www.google.com/search?tbm=isch&q='+thing
-r = requests.get(url)
-html_content = r.text
-soup = BeautifulSoup(html_content,'lxml')
-print(soup.a)
