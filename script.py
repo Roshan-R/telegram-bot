2edcec040d3ac2c9055ff044f2d7e068df813a0e
Sudev Suresh Sreedevi <54617167+GameGodS3@users.noreply.github.com>
2020-12-02 16:22:40 +0530

    Chane to DuckDuckGo and exception handling

--- script.py
+++ script.py
@@ -12,13 +12,16 @@ bot = telebot.TeleBot(TG_BOT_TOKEN)
 
 @bot.message_handler(func=lambda m: True)
 def image(message):
-    print("Query : ",message.text)
-    urlopenheader={ 'User-Agent' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
-    request_url='https://www.bing.com/images/async?q=' + message.text.replace(' ','%20') + '&count=35&adlt=off'
-    request=urllib.request.Request(request_url,None,headers=urlopenheader)
-    response=urllib.request.urlopen(request)
-    html = response.read().decode('utf8')
-    links = re.findall('murl&quot;:&quot;(.*?)&quot;',html)
-    print(links[0])
-    bot.send_photo(chat_id,links[0])
+    try:
+        print("Query : ",message.text)
+        urlopenheader={ 'User-Agent' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+        request_url='https://duckduckgo.com/?q=' + message.text.replace(' ','+') + '&t=h_&iar=images&iax=images&ia=images'
+        request=urllib.request.Request(request_url,None,headers=urlopenheader)
+        response=urllib.request.urlopen(request)
+        html = response.read().decode('utf8')
+        links = re.findall('murl&quot;:&quot;(.*?)&quot;',html)
+        print(links[0])
+        bot.send_photo(chat_id,links[0])
+    except Excpetion as err:
+        pass
 bot.polling(none_stop=True,timeout=123)
